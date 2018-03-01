f7343e65e75fe51fa74b196273a6751adbc04da7
Ariel Virgulto <ariel.virgulto@gmail.com>
2018-03-01 17:28:58 -0500

    Changed user interface layout a little bit, added two drop down menus

--- app/src/main/java/com/synchrony/uconn/design/synchronyar/UserInterface.java
+++ app/src/main/java/com/synchrony/uconn/design/synchronyar/UserInterface.java
@@ -3,7 +3,9 @@ package com.synchrony.uconn.design.synchronyar;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class UserInterface extends Activity {
 
@@ -19,6 +21,24 @@ public class UserInterface extends Activity {
                 finish();
             }
         });
+
+        Spinner spinner_color = (Spinner) findViewById(R.id.spinner_colors);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Colors, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_color.setPrompt("Select for Colors");
+
+        Spinner spinner_size = (Spinner) findViewById(R.id.spinner_size);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.Sizes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_color.setPrompt("Select for sizes");
+
+/*
+        spinner.setAdapter(
+                new NothingSelectedSpinnerAdapter(
+                        adapter,
+                        R.layout.contact_spinner_row_nothing_selected,
+                        // R.layout.contact_spinner_nothing_selected_dropdown, // Optional
+                        this));*/
     }
 
 }
